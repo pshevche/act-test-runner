f4fcf7fa59b7a748f34e1547d75d8e52aebfff0d
Pavlo Shevchenko <pavel.shevchenko.95@gmail.com>
2025-03-30 14:34:29 +0200

    Migrate to eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,32 @@
+import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import tsparser from '@typescript-eslint/parser';
+import prettier from 'eslint-plugin-prettier';
+
+export default [
+  js.configs.recommended,
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        project: './tsconfig.json',
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+      prettier: prettier,
+    },
+    rules: {
+      'no-debugger': 'warn',
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': 'warn',
+    },
+    settings: {
+      jest: true,
+      node: true,
+    },
+  },
+];
