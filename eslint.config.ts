14ce51be23123be6d848e2f24e0d710b863d6094
Pavlo Shevchenko <pavel.shevchenko.95@gmail.com>
2025-03-30 21:08:52 +0200

    Fix up lint check

--- eslint.config.ts
+++ eslint.config.ts
@@ -7,6 +7,7 @@ export default [
   js.configs.recommended,
   {
     files: ['**/*.ts', '**/*.tsx'],
+    ignores: ['dist/**', 'node_modules/**'],
     languageOptions: {
       parser: tsparser,
       parserOptions: {
