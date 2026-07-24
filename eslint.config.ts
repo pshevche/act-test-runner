7639a6e92d403f9a3f82d1f68d451c9e9fdd836f
Pavlo Shevchenko <pavel.shevchenko.95@gmail.com>
2026-07-24 22:47:43 +0200

    build: migrate from npm to pnpm

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,36 +1,30 @@
-import tseslint from '@typescript-eslint/eslint-plugin';
-import tsparser from '@typescript-eslint/parser';
+import { defineConfig } from 'eslint/config';
+import tseslint from 'typescript-eslint';
 import prettier from 'eslint-plugin-prettier';
 import globals from 'globals';
 
-export default [
-  {
-    files: ['**/*.ts', '**/*.tsx'],
-    ignores: ['dist/**', 'node_modules/**'],
-    languageOptions: {
-      parser: tsparser,
-      parserOptions: {
-        project: './tsconfig.json',
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-      },
-      globals: {
-        ...globals.node,
-      },
+export default defineConfig({
+  files: ['**/*.ts', '**/*.tsx'],
+  ignores: ['dist/**', 'node_modules/**'],
+  languageOptions: {
+    parser: tseslint.parser,
+    parserOptions: {
+      projectService: true,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
     },
-    plugins: {
-      '@typescript-eslint': tseslint,
-      prettier: prettier,
-    },
-    rules: {
-      'no-unused-vars': 'off',
-      'no-debugger': 'warn',
-      'prettier/prettier': 'error',
-      '@typescript-eslint/no-unused-vars': 'warn',
-    },
-    settings: {
-      jest: true,
-      node: true,
+    globals: {
+      ...globals.node,
     },
   },
-];
+  plugins: {
+    '@typescript-eslint': tseslint.plugin,
+    prettier,
+  },
+  rules: {
+    'no-unused-vars': 'off',
+    'no-debugger': 'warn',
+    'prettier/prettier': 'error',
+    '@typescript-eslint/no-unused-vars': 'warn',
+  },
+});
