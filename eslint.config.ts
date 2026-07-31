1d3c6e2a570cc751108488d6976fcb40b2403d36
Pavlo Shevchenko <pavel.shevchenko.95@gmail.com>
2026-07-31 09:54:11 +0200

    build: enforce license header checks with ESLint (#144)
    * build: enforce the license header via eslint
    
    * chore: bump the year in the license header
    
    * fix: mute ts error for yet-another-license-header plugin

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,29 +2,65 @@ import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 import prettier from 'eslint-plugin-prettier';
 import globals from 'globals';
+// @ts-expect-error - plugin ships no types
+import headerPlugin from 'eslint-plugin-yet-another-license-header';
 
-export default defineConfig({
-  files: ['**/*.ts', '**/*.tsx'],
-  ignores: ['dist/**', 'node_modules/**'],
-  languageOptions: {
-    parser: tseslint.parser,
-    parserOptions: {
-      projectService: true,
-      ecmaVersion: 'latest',
-      sourceType: 'module',
+const licenseHeader = `
+/**
+ * Copyright (c) 2026 original authors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+ * FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+ * IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+`;
+
+export default defineConfig([
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        projectService: true,
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      globals: {
+        ...globals.node,
+      },
     },
-    globals: {
-      ...globals.node,
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+      prettier,
+      'yet-another-license-header': headerPlugin,
+    },
+    rules: {
+      'no-unused-vars': 'off',
+      'no-debugger': 'warn',
+      'prettier/prettier': 'error',
+      '@typescript-eslint/no-unused-vars': 'warn',
     },
   },
-  plugins: {
-    '@typescript-eslint': tseslint.plugin,
-    prettier,
+  {
+    files: ['src/**/*.ts', 'src/**/*.tsx'],
+    rules: {
+      'yet-another-license-header/header': ['error', { header: licenseHeader }],
+    },
   },
-  rules: {
-    'no-unused-vars': 'off',
-    'no-debugger': 'warn',
-    'prettier/prettier': 'error',
-    '@typescript-eslint/no-unused-vars': 'warn',
-  },
-});
+]);
